f437788c309f7296e8daf62da01c7bc4ebae8753
Joshua Hoblitt <josh@hoblitt.com>
2015-07-14 09:41:56 -0700

    ping vagrant version to 1.7.2
    Due to 1.7.3 breaking provision overrides:
        https://github.com/mitchellh/vagrant/issues/5958

--- site/manifests/role/leo.pp
+++ site/manifests/role/leo.pp
@@ -3,7 +3,8 @@ class site::role::leo {
   include ::site::profile::desktop
   include ::site::profile::ssh
   include ::virtualbox
-  include ::vagrant
+  # https://github.com/mitchellh/vagrant/issues/5958
+  class { '::vagrant': version => '1.7.2' }
   include ::smartd
   include ::mcelog
 }
